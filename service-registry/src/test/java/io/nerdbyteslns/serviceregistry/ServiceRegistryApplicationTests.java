acfa16e1aa7e2918a796bbc1301f1a4dbf943576
Matthew Oduamafu <mattoduamafu@gmail.com>
2024-08-15 15:42:12 +0000

    add service discovery

---
+++ service-registry/src/test/java/io/nerdbyteslns/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nerdbyteslns.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
